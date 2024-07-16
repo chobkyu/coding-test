2753c992902c532277961f27651c30aae144b37d
chobkyu <qudqud97@naver.com>
2024-07-16 23:28:48 +0900

    Create LinkedList.ts

---
+++ Data Structures/Linked list/LinkedList.ts
@@ -0,0 +1,42 @@
+// class LinkedNode extends Node {
+//     item: number;
+//     next: Node | null = null;
+
+//     constructor(item: number) {
+//         this.item = item;
+//     }
+
+// }
+
+// class linkedList {
+//     private head: LinkedNode|null = null;
+//     private length: number = 0;
+
+//     /** Iterates through the linked list and outputs the data of the node.*/
+//     traverse() {
+//         let current = this.head;
+//         let result = "";
+
+//         while(current) {
+//             result += current.item + " -> ";
+//             current = current.next;
+//         }
+
+//     }
+
+//     /**Add a new node to the end of the linked list */
+//     append(node: Node) {
+
+//     }
+
+//     /*Add a new node to the middle of the linked list */
+//     insertAt(idx: number, node: Node) {
+
+//     }
+
+//     /**Delete a specific node in the middle of a linked list. */
+//     deleteAt(idx: number) {
+
+//     }
+
+// }
